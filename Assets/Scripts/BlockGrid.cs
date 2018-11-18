2b060233812361fdc82c12a918cc2633fbf1fe8e
Dmitriy <4aynoedepebo@gmail.com>
2018-11-18 15:01:27 +0200

    pre level loading

---
+++ Assets/Scripts/BlockGrid.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGrid : MonoBehaviour {
+
+    private Block[] _blocks;
+
+	// This will contain the properties and methods of the Block Grid
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LevelData {
+    public string[,] data;
+}
+
+public class LevelLoader {
+
+    private List<LevelData> _levels;
+
+    public void Load() {
+
+    }
+
+    // Here is where we are going to be loading levels from JSON, processign them and populating LevelData with parced JSON data
+}
