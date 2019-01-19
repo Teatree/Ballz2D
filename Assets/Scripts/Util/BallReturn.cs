c397f0dcf89ffd0a006c49c97d5b197ff680e775
Dmitriy <4aynoedepebo@gmail.com>
2019-01-19 15:58:36 +0200

    Balls now fly wih physics

---
+++ Assets/Scripts/Util/BallReturn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        collision.collider.gameObject.GetComponent<Ball>().OnFloorCollision(collision.collider);
+    }
+}
