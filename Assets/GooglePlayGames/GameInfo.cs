ba6754d452d0ba34bba55f11b227553a1f43085c
Dmitriy <4aynoedepebo@gmail.com>
2019-04-06 13:28:59 +0300

    Pre-Commit
     * a safe commit

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "912453684403"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "912453684403-cvehgr51suqh9vea99pdgfgkmf9316io.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
