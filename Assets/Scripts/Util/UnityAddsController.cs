3c35199e93dd1aadd931027e52dd43b425d64b67
Dmitriy <4aynoedepebo@gmail.com>
2019-04-25 13:16:05 +0300

    unity ads from 'goHere' folder
    unity ads from 'goHere' folder

---
+++ Assets/Scripts/Util/UnityAddsController.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections;
+using UnityEngine;
+
+using UnityEngine.Monetization;
+
+
+public class UnityAddsController : SceneSingleton<UnityAddsController> {
+    private string gameId = "3108568";
+    private string boxAd = "BoxRewarded";
+    private string moreHCAd = "MoreHCRewarded";
+
+    public int AdBoxOpenLimit = 3;
+
+    void Start() {
+        UIController.Instance.SetEnabledAdBox(false);
+        if (Monetization.isSupported && PlayerController.player.adBoxOpenedCount < AdBoxOpenLimit) {
+            Monetization.Initialize(gameId, true);
+        }
+    }
+
+    public void ShowMoreHCReviveAd() {
+        ShowAdCallbacks options = new ShowAdCallbacks();
+        options.finishCallback = HandleMoreHCGiveNastya;
+        ShowAdPlacementContent ad = Monetization.GetPlacementContent(moreHCAd) as ShowAdPlacementContent;
+        ad.Show(options);
+
+        PlayerController.player.MoreHCReviveCount++;
+        ((Revive)Revive.Instance).UpdateButtsButtonState();
+    }
+
+    public void ShowBoxAd() {
+        DateTime dt = PlayerController.player == null || PlayerController.player.adBoxOpenedDate == null || PlayerController.player.adBoxOpenedDate == "" ? DateTime.Now : DateTime.Parse(PlayerController.player.adBoxOpenedDate);
+
+        if (DateTime.Now.Date == dt) {
+            // if Player came logged in after box was supposed to be claiemd
+
+            // Not exceding limit
+            if(PlayerController.player.adBoxOpenedCount < AdBoxOpenLimit) {
+                ShowAdCallbacks options = new ShowAdCallbacks();
+                options.finishCallback = HandleOpenAdBoxNastya;
+                ShowAdPlacementContent ad = Monetization.GetPlacementContent(boxAd) as ShowAdPlacementContent;
+                ad.Show(options);
+
+                PlayerController.player.adBoxOpenedCount++;
+            }
+            else {
+                UIController.Instance.SetEnabledAdBox(false);
+            }
+        }
+        else {
+            PlayerController.player.adBoxOpenedDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
+
+            ShowAdCallbacks options = new ShowAdCallbacks();
+            options.finishCallback = HandleOpenAdBoxNastya;
+            ShowAdPlacementContent ad = Monetization.GetPlacementContent(boxAd) as ShowAdPlacementContent;
+            ad.Show(options);
+
+            PlayerController.player.adBoxOpenedCount = 1;
+        }
+        // set the dat of ad shown
+        // Add counter to ad shown
+    }
+
+    void HandleOpenAdBoxNastya(ShowResult result) {
+        if (result == ShowResult.Finished) {
+            Debug.LogWarning(" REWARD!");
+            UIController.Instance.OpenBoxOpen();
+        }
+        else if (result == ShowResult.Skipped) {
+            Debug.LogWarning("The player skipped the video - DO NOT REWARD!");
+        }
+        else if (result == ShowResult.Failed) {
+            Debug.LogError("Video failed to show");
+        }
+    }
+
+    void HandleMoreHCGiveNastya(ShowResult result) {
+        if (result == ShowResult.Finished) {
+            Debug.LogWarning(" REWARD!");
+            GameUIController.Instance.ShowItemReceived();
+            PlayerController.player.gems += 40;
+        }
+        else if (result == ShowResult.Skipped) {
+            Debug.LogWarning("The player skipped the video - DO NOT REWARD!");
+        }
+        else if (result == ShowResult.Failed) {
+            Debug.LogError("Video failed to show");
+        }
+    }
+}
---
+++ Assets/Scripts/Util/UnityBanner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class UnityBanner : MonoBehaviour {
+
+    private string banner = "Banner";
+    public bool testMode = false;
+
+#if UNITY_ANDROID
+    public const string gameId = "3108568";
+#elif UNITY_EDITOR
+    public const string gameId = "1111111";
+#endif
+
+    // Use this for initialization
+    void Start() {
+        Advertisement.Initialize(gameId, testMode);
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+       
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady() {
+        while (!Advertisement.IsReady(banner)) {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(banner);
+    }
+}
